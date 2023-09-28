faacfe27709fa23b254f1b90692d0d52c75ebf97
Ken Fiskorne <fiskorne.fireback@gmail.com>
2023-09-28 17:53:00 -0400

    tweaks

--- src/main/java/com/github/hellocrossy/wondersoftheworld/client/model/EmuModel.java
+++ src/main/java/com/github/hellocrossy/wondersoftheworld/client/model/EmuModel.java
@@ -214,6 +214,10 @@ public abstract class EmuModel extends ZawaBaseModel<EmuEntity> {
         @Override
         public void setupAnim(EmuEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
             super.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+            this.Neck.yRot = netHeadYaw / (180F / (float) Math.PI) * 0.25F;
+            this.Head.yRot = netHeadYaw / (180F / (float) Math.PI) * 0.25F;
+            this.Head.xRot = (headPitch / (180F / (float) Math.PI)) + 0.117F;
+            this.Head.zRot = headPitch / (180F / (float) Math.PI) * 0.05F;
         }
 
         @Override
