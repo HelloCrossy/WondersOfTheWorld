0aafa593404344974c9ddbf5c8c002eea9a56a78
Ken Fiskorne <fiskorne.fireback@gmail.com>
2023-10-01 13:44:51 -0400

    emu walk, kiwi idle

--- src/main/java/com/github/hellocrossy/wondersoftheworld/client/model/KiwiModel.java
+++ src/main/java/com/github/hellocrossy/wondersoftheworld/client/model/KiwiModel.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableList;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 import org.zawamod.zawa.client.model.ZawaBaseModel;
 import org.zawamod.zawa.client.model.ZawaModelRenderer;
 
@@ -142,11 +143,19 @@ public abstract class KiwiModel extends ZawaBaseModel<KiwiEntity> {
         @Override
         public void setupAnim(KiwiEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
             super.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+            this.Neck.yRot = netHeadYaw / (180F / (float) Math.PI) * 0.25F;
+            this.Head.yRot = netHeadYaw / (180F / (float) Math.PI) * 0.25F;
+        //    this.Head.xRot = (headPitch / (180F / (float) Math.PI)) + 0F;
+            this.Head.zRot = headPitch / (180F / (float) Math.PI) * 0.05F;
         }
 
         @Override
         public void playIdleAnimation(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
+            float speed = 1.0f;
+            float degree = 1.0f;
+            this.Chest.xRot = MathHelper.cos(6.0F + limbSwing * speed * 0.07F) * limbSwingAmount * (degree * -0.2F) * 0.5F + 0.917F;
+            this.Neck.xRot = MathHelper.cos(2.0F + limbSwing * speed * 0.07F) * limbSwingAmount * (degree * 0.2F) * 0.5F - 0.682F;
+            this.Head.xRot = MathHelper.cos(4.0F + limbSwing * speed * 0.07F) * limbSwingAmount * (degree * -0.15F) * 0.5F;
         }
 
         @Override
