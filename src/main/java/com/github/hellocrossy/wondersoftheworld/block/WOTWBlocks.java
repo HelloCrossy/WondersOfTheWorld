546f6c00520e5feb8d558a98a7b9e8100c362895
HelloCrossy <117866534+HelloCrossy@users.noreply.github.com>
2024-07-08 20:39:00 +0100

    Sous Chef - Block Fix

--- src/main/java/com/github/hellocrossy/wondersoftheworld/block/WOTWBlocks.java
+++ src/main/java/com/github/hellocrossy/wondersoftheworld/block/WOTWBlocks.java
@@ -2,10 +2,9 @@ package com.github.hellocrossy.wondersoftheworld.block;
 
 import com.github.hellocrossy.wondersoftheworld.WondersOfTheWorld;
 import com.github.hellocrossy.wondersoftheworld.item.WOTWItems;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.BlockItem;
@@ -13,6 +12,7 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Util;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -24,22 +24,32 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 import static com.github.hellocrossy.wondersoftheworld.WondersOfTheWorld.PLUSHIES_LIST;
+import static net.minecraft.item.ItemModelsProperties.register;
 
 public class WOTWBlocks {
     public static final DeferredRegister<Block> REGISTRAR = DeferredRegister.create(ForgeRegistries.BLOCKS, WondersOfTheWorld.MOD_ID);
 
-    public static final RegistryObject<Block> CRACKED_STONE = registerWithItem("cracked_stone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> MOSSY_CRACKED_STONE = registerWithItem("mossy_cracked_stone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> LIGHT_CRACKED_STONE = registerWithItem("light_cracked_stone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> DESERT_CRACKED_STONE = registerWithItem("desert_cracked_stone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> MESA_CRACKED_STONE = registerWithItem("mesa_cracked_stone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> ARCTIC_CRACKED_STONE = registerWithItem("arctic_cracked_stone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> BREEZE_BLOCK = registerWithItem("breeze_block", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> DEFINED_BREEZE_BLOCK = registerWithItem("defined_breeze_block", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> FINE_DIRT = registerWithItem("fine_dirt", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.SAND).sound(SoundType.SAND)));
-    public static final RegistryObject<Block> FINE_SAND = registerWithItem("fine_sand", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.SAND).sound(SoundType.SAND)));
-    public static final RegistryObject<Block> DRY_COBBLESTONE = registerWithItem("dry_cobblestone", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> RED_BRICK = registerWithItem("red_brick", () -> new RotationalBlock(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CRACKED_STONE = registerWithItem("cracked_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(1.5F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MOSSY_CRACKED_STONE = registerWithItem("mossy_cracked_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(1.5F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> LIGHT_CRACKED_STONE = registerWithItem("light_cracked_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(1.5F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DESERT_CRACKED_STONE = registerWithItem("desert_cracked_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(1.5F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MESA_CRACKED_STONE = registerWithItem("mesa_cracked_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(1.5F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> ARCTIC_CRACKED_STONE = registerWithItem("arctic_cracked_stone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(1.5F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> BREEZE_BLOCK = registerWithItem("breeze_block", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(2.0F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> BREEZE_BLOCK_SLAB = registerWithItem("breeze_block_slab", () -> {return new StairsBlock(() -> {return ((Block)BREEZE_BLOCK.get()).defaultBlockState();}, AbstractBlock.Properties.copy((AbstractBlock)BREEZE_BLOCK.get()));});
+    public static final RegistryObject<Block> BREEZE_BLOCK_STAIRS = registerWithItem("breeze_block_stairs", () -> {return new StairsBlock(() -> {return ((Block)BREEZE_BLOCK.get()).defaultBlockState();}, AbstractBlock.Properties.copy((AbstractBlock)BREEZE_BLOCK.get()));});
+    public static final RegistryObject<Block> BREEZE_BLOCK_WALL = registerWithItem("breeze_block_wall", () -> {return new WallBlock(AbstractBlock.Properties.copy((AbstractBlock)BREEZE_BLOCK.get()));});
+    public static final RegistryObject<Block> DEFINED_BREEZE_BLOCK = registerWithItem("defined_breeze_block", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(2.0F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DEFINED_BREEZE_BLOCK_SLAB = registerWithItem("defined_breeze_block_slab", () -> {return new StairsBlock(() -> {return ((Block)DEFINED_BREEZE_BLOCK.get()).defaultBlockState();}, AbstractBlock.Properties.copy((AbstractBlock)DEFINED_BREEZE_BLOCK.get()));});
+    public static final RegistryObject<Block> DEFINED_BREEZE_BLOCK_STAIRS = registerWithItem("defined_breeze_block_stairs", () -> {return new StairsBlock(() -> {return ((Block)DEFINED_BREEZE_BLOCK.get()).defaultBlockState();}, AbstractBlock.Properties.copy((AbstractBlock)DEFINED_BREEZE_BLOCK.get()));});
+    public static final RegistryObject<Block> DEFINED_BREEZE_BLOCK_WALL = registerWithItem("defined_breeze_block_wall", () -> {return new WallBlock(AbstractBlock.Properties.copy((AbstractBlock)DEFINED_BREEZE_BLOCK.get()));});
+    public static final RegistryObject<Block> RED_BRICK = registerWithItem("red_brick", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(2.0F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> RED_BRICK_SLAB = registerWithItem("red_brick_slab", () -> {return new StairsBlock(() -> {return ((Block)RED_BRICK.get()).defaultBlockState();}, AbstractBlock.Properties.copy((AbstractBlock)RED_BRICK.get()));});
+    public static final RegistryObject<Block> RED_BRICK_STAIRS = registerWithItem("red_brick_stairs", () -> {return new StairsBlock(() -> {return ((Block)RED_BRICK.get()).defaultBlockState();}, AbstractBlock.Properties.copy((AbstractBlock)RED_BRICK.get()));});
+    public static final RegistryObject<Block> RED_BRICK_WALL = registerWithItem("red_brick_wall", () -> {return new WallBlock(AbstractBlock.Properties.copy((AbstractBlock)RED_BRICK.get()));});
+    public static final RegistryObject<Block> FINE_DIRT = registerWithItem("fine_dirt", () -> new Block(AbstractBlock.Properties.of(Material.SAND).sound(SoundType.SAND).strength(0.5F).harvestTool(ToolType.SHOVEL)));
+    public static final RegistryObject<Block> FINE_SAND = registerWithItem("fine_sand", () -> new Block(AbstractBlock.Properties.of(Material.SAND).sound(SoundType.SAND).strength(0.5F).harvestTool(ToolType.SHOVEL)));
+    public static final RegistryObject<Block> DRY_COBBLESTONE = registerWithItem("dry_cobblestone", () -> new Block(AbstractBlock.Properties.of(Material.STONE).sound(SoundType.STONE).strength(2.0F, 6.0F).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()));
     public static final Map<String, RegistryObject<Block>> PLUSHIES = Util.make(new HashMap<>(), map -> {
         for (String plush : PLUSHIES_LIST)
             map.put(plush, REGISTRAR.register(plush + "_plush", () -> new PlushBlock(AbstractBlock.Properties.of(Material.WOOL).sound(SoundType.WOOL).noOcclusion())));
